3b57e079d40bfd4f3bbfb63a9b4ddf1c8a788445
Nan <60449376+minamiatsuko@users.noreply.github.com>
2020-06-04 20:14:14 +0200

    Add files via upload

---
+++ menu Project/public/js/main.js
@@ -0,0 +1,18 @@
+(function() {
+
+  'use strict';
+
+  var show= document.getElementById('show');
+  var main=document.getElementById('main');
+
+  show.addEventListener('click', function() {
+    if (main.className ==='menu-open') {
+      main.className = '';
+    } 
+    else{
+      main.className ='menu-open' ;
+    }
+  });
+
+})();
+
---
+++ menu Project2/public/js/main2.js
@@ -0,0 +1,16 @@
+(function() {
+
+  'use strict';
+
+  var show= document.getElementById('show');
+  var hide= document.getElementById('hide');
+  
+
+  show.addEventListener('click', function() {
+    document.body.className='menu-open';
+  });
+  
+  hide.addEventListener('click', function() {
+    document.body.className='';
+  });
+})();
